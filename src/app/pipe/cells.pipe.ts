8432487f2bcf5078d6e0e1879c52d7c2b33ba5b9
telomere <stephane.schwenker@uha.fr>
2018-07-19 08:27:10 +0200

    deux filtre avec pipe enchainé en fonction, le passage de plusieurs paramètre ne fonctionne pas donc il faut enchainer les pipes

--- src/app/pipe/cells.pipe.ts
+++ src/app/pipe/cells.pipe.ts
@@ -3,10 +3,26 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'cells'
 })
-export class CellsPipe implements PipeTransform {
 
+export class CellsPipe implements PipeTransform {
+  // Transform is the new "return function(value, args)" in Angular 1.x
   transform(value: any, args?: any): any {
-    return null;
+    // ES6 array destructuring
+    let [minCells] = args
+    console.log('args cells' + args);
+    console.log(value);
+    return value.filter(telomere => {
+
+      /*if (args == '') {
+        return true;
+      }*/
+
+     console.log("true or false " + (telomere.originalname===args));
+     console.log("true or false " + (telomere.originalname==args));
+        return telomere.originalname == args;
+      
+
+    });
   }
 
 }
--- src/app/pipe/nbCells-pipe.ts
+++ src/app/pipe/nbCells-pipe.ts
@@ -7,17 +7,18 @@ import {Pipe} from '@angular/core';
 export class nbCellsPipe {
 
   // Transform is the new "return function(value, args)" in Angular 1.x
-  transform(value: any, args? :any) : any {
+  transform(value: any, args1? :any) : any {
     // ES6 array destructuring
-    let [minCells] = args
-    console.log(value);
+    let [minCells] = args1 //undefined
+    
     return value.filter(telomere => {
         let g=Number(telomere.nbCells)
-        console.log((g <= 1 )+ ' true or false') ;
+        
 
 
-        console.log(g<=args);
-    return   Number(telomere.nbCells) <= args;
+        console.log(g<=args1);
+   // return   Number(telomere.nbCells) <= args1 ;
+    return g<=args1;
     });
   }
 
